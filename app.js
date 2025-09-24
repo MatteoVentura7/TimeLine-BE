106c92d9aa616c3ef1b23c6a02d94667131ae0a5
MatteoVentura7 <matteo.ventura18@gmail.com>
2025-09-24 12:21:16 +0200

    connect to db

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
     const app = express(); // Crea un'istanza dell'applicazione Express
     const port = 3000; // Definisce la porta su cui il server sarà in ascolto
 
+    const db = require("./db.js");
+    const cors = require("cors");
+    // Usa il middleware CORS
+    app.use(cors());
+    // Middleware per il parsing del body in formato JSON
+    app.use(express.json());
+
     // Definisci una rotta per la homepage (il percorso "/")
     app.get('/', (req, res) => {
       res.send('Benvenuto nella tua prima applicazione Express!'); // Invia una risposta al client
@@ -11,3 +18,4 @@
     app.listen(port, () => {
       console.log(`Server in ascolto su http://localhost:${port}`); // Messaggio in console quando il server è avviato
     });
+
---
+++ db.js
@@ -0,0 +1,32 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "time-line-db",
+});
+
+function connectToDatabase() {
+  connection.connect((err) => {
+    if (err) {
+      console.error("Errore di connessione al database:", err.message);
+      setTimeout(connectToDatabase, 2000); // Riprova la connessione dopo 2 secondi
+    } else {
+      console.log("Connesso a MySQL!");
+    }
+  });
+
+  connection.on("error", (err) => {
+    console.error("Errore del database:", err.message);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      connectToDatabase(); // Riconnessione automatica
+    } else {
+      throw err;
+    }
+  });
+}
+
+connectToDatabase();
+
+module.exports = connection;
