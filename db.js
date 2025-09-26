574b39d096461e61cb3cd2c0e4fc61423c02f3ac
MatteoVentura <matteo.ventura18@gmail.com>
2025-09-26 12:11:46 +0200

    fix connect to db

--- db.js
+++ db.js
@@ -3,27 +3,14 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "13579Matteo",
   database: "time-line-db",
 });
 
 function connectToDatabase() {
   connection.connect((err) => {
-    if (err) {
-      console.error("Errore di connessione al database:", err.message);
-      setTimeout(connectToDatabase, 2000); // Riprova la connessione dopo 2 secondi
-    } else {
-      console.log("Connesso a MySQL!");
-    }
-  });
-
-  connection.on("error", (err) => {
-    console.error("Errore del database:", err.message);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      connectToDatabase(); // Riconnessione automatica
-    } else {
-      throw err;
-    }
+    if (err) throw err;
+    console.log("Connected to MySql");
   });
 }
 
